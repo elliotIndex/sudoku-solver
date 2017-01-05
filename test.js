27c4b188faaa498dcb5c5aa5010e9e2b5fb91143
Elliot Plant <elliotaplant@gmail.com>
2017-01-04 20:29:37 -0800

    more

--- test.js
+++ test.js
@@ -36,12 +36,28 @@ const testBoard3 = [
   [0,0,5,0,4,7,8,3,0],
 ];
 
-const solved1 = solveSudoku(testBoard);
-console.log(verify(solved1) ? "Valid" : "Invalid");
+// hard
+const testBoard4 = [
+  [0,0,4,2,0,0,0,7,5],
+  [0,0,0,9,1,0,0,0,4],
+  [0,3,0,0,0,0,9,0,0],
+  [9,0,1,0,7,0,4,0,2],
+  [0,0,8,0,0,0,0,1,0],
+  [0,0,0,5,0,0,0,0,1],
+  [6,0,0,0,4,3,0,0,0],
+  [5,7,0,0,0,1,6,0,0],
+];
 
-const solved2 = solveSudoku(testBoard2);
-console.log(verify(solved2) ? "Valid" : "Invalid");
+// const solved1 = solveSudoku(testBoard);
+// console.log(verify(solved1) ? "Valid" : "Invalid");
+//
+// const solved2 = solveSudoku(testBoard2);
+// console.log(verify(solved2) ? "Valid" : "Invalid");
+//
+// const solved3 = solveSudoku(testBoard3);
+// printBoard(solved3);
+// console.log(verify(solved3) ? "Valid" : "Invalid");
 
-const solved3 = solveSudoku(testBoard3);
-printBoard(solved3);
-console.log(verify(solved3) ? "Valid" : "Invalid");
+const solved4 = solveSudoku(testBoard4);
+printBoard(solved4);
+console.log(verify(solved4) ? "Valid" : "Invalid");
